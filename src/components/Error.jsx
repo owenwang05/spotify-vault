e71db0e80fdafcd340528b13613367edf5ac98b5
owen-wang-student <owang420@gmail.com>
2024-05-17 02:33:18 -0500

    implemented spotify oauth

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+export function Error() {
+  return (
+    <div className='bg-background-primary h-screen w-screen'>
+      <h2 className='text-t-primary text-l'>Invalid Page</h2>
+    </div>
+  );
+}
