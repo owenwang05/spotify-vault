dd78b20a7d297ff64832cb1008eaafdda812826f
xu.z.brandon@gmail.com <68095440+bxrr@users.noreply.github.com>
2024-06-02 19:49:47 -0700

    removed unnecessary JSON return value

--- backend/userdata/serializers.py
+++ backend/userdata/serializers.py
@@ -4,4 +4,4 @@ from userdata.models import Snapshot
 class SnapshotSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snapshot
-        fields = ['id', 'snapshot_date', 'listening_time', 'top_genres', 'song_ids', 'artist_ids']
+        fields = ['snapshot_date', 'listening_time', 'top_genres', 'song_ids', 'artist_ids']
